2c0f3e638b4b5f1cd77cad209409962fd26ef1c4
Sleightttt <brennancota@yahoo.com>
2024-01-03 13:33:25 -0800

    added dropdown and dropdown animations

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
+import hamburger from "../../icons/menu.png";
 
 const Navbar = () => {
+  const [isDropdownVisible, setDropdownVisibility] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisibility(!isDropdownVisible);
+  };
+
   return (
     <>
       <div className="nav-container">
         <div className="logo">OOTify</div>
-        <div>Higher Ed</div>
-        <div>Enterprise</div>
-        <div>Hospitals</div>
-        <div>Children's Book</div>
-        <div>Our Story</div>
+        <div className="anchor-container">
+          <div className="anchor-link">Higher Ed</div>
+          <div className="anchor-link">Enterprise</div>
+          <div className="anchor-link">Hospitals</div>
+          <div className="anchor-link">Children's Book</div>
+          <div className="anchor-link">Our Story</div>
+          <div
+            className={`hamburger ${isDropdownVisible ? "active" : ""}`}
+            onClick={toggleDropdown}
+          >
+            <img
+              className="hamburger-icon"
+              alt="nav-hamburger"
+              src={hamburger}
+            />
+            <div
+              className={`dropdown ${
+                isDropdownVisible ? "slideInFromTop" : "slideOutToTop"
+              }`}
+            >
+              <div className="dropdown-link">
+                <span>Higher Ed</span>
+              </div>
+              <div className="dropdown-link">
+                {" "}
+                <span>Enterprise</span>
+              </div>
+              <div className="dropdown-link">
+                {" "}
+                <span>Hospitals</span>
+              </div>
+              <div className="dropdown-link">
+                {" "}
+                <span>Children's Book</span>
+              </div>
+              <div className="dropdown-link">
+                {" "}
+                <span>Our Story</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
