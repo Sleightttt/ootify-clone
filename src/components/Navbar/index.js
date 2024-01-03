a2f23c7e4a9530ebae540f8fef3eb2386606aefc
Sleightttt <brennancota@yahoo.com>
2024-01-03 12:29:19 -0800

    styled nav bar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav-container">
+        <div className="logo">OOTify</div>
+        <div>Higher Ed</div>
+        <div>Enterprise</div>
+        <div>Hospitals</div>
+        <div>Children's Book</div>
+        <div>Our Story</div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
