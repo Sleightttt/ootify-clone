01cde4a7b25cfcd3ef2fbc67b86d6862bd565923
Sleightttt <brennancota@yahoo.com>
2024-01-04 22:14:32 -0800

    fixed linter errors for >

--- src/components/Whowehelp/index.js
+++ src/components/Whowehelp/index.js
@@ -12,21 +12,21 @@ const Whowehelp = () => {
             <div className="who-body">
               On-demand, personalized mental health support for students.
             </div>
-            <div className="learn">Learn More &nbsp;></div>
+            <div className="learn">Learn More &nbsp;&gt;</div>
           </div>
           <div className="who-card">
             <div className="card-header">Enterprise</div>
             <div className="who-body">
               High-quality, low cost, virtual support for every employee.
             </div>
-            <div className="learn">Learn More &nbsp;></div>
+            <div className="learn">Learn More &nbsp;&gt;</div>
           </div>
           <div className="who-card">
             <div className="card-header">Hospitals</div>
             <div className="who-body">
               Clinical and sub-clinical support for your patients to thrive.
             </div>
-            <div className="learn">Learn More &nbsp;></div>
+            <div className="learn">Learn More &nbsp;&gt;</div>
           </div>
         </div>
       </section>
