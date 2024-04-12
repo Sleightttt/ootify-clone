11474fca8f6e8e2e38fc43bf87128a392b79d385
Sleightttt <brennancota@yahoo.com>
2024-04-11 17:01:24 -0700

    reorganized project directories

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Introduction from "./components/Introduction";
-import Mentions from "./components/Mentions";
-import Bestfriend from "./components/Bestfriend";
-import Mental from "./components/Mental";
-import Notsure from "./components/Notsure";
-import How from "./components/How";
-import Whowehelp from "./components/Whowehelp";
-import Tools from "./components/Tools";
-import Ourteam from "./components/Ourteam";
-import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+
 import "./app.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Introduction />
-        <div className="blue-bg">
-          <Mentions />
-          <Bestfriend />
-          <Mental />
-          <Notsure />
-        </div>
-        <How />
-        <Whowehelp />
-        <Tools />
-        <Ourteam />
-        <Footer />
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,29 @@
+import Introduction from "../LandingPage/Introduction";
+import Mentions from "../LandingPage/Mentions";
+import Bestfriend from "../LandingPage/Bestfriend";
+import Mental from "../LandingPage/Mental";
+import Notsure from "../LandingPage/Notsure";
+import How from "../LandingPage/How";
+import Whowehelp from "../LandingPage/Whowehelp";
+import Tools from "../LandingPage/Tools";
+import Ourteam from "../LandingPage/Ourteam";
+import Footer from "../LandingPage/Footer";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Introduction />
+      <Mentions />
+      <Bestfriend />
+      <Mental />
+      <Notsure />
+      <How />
+      <Whowehelp />
+      <Tools />
+      <Ourteam />
+      <Footer />
+    </>
+  );
+};
+
+export default LandingPage;
