7b285d10f2c8ecec0a4d3dd8fba416f6c19ccfde
Sleightttt <brennancota@yahoo.com>
2024-04-13 17:10:30 -0700

    adding higher ed page

--- src/components/HigherEd/index.js
+++ src/components/HigherEd/index.js
@@ -17,8 +17,222 @@ const HigherEd = () => {
                 students to help schools improve retention, diversity, and
                 overall student success.
               </div>
+              <button className="begin-button2">Begin Your Journey</button>
             </div>
-            <div className="he-title-image"></div>
+            <div className="he-title-image">
+              <img
+                alt="icon"
+                className="he-title-img"
+                src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a24a59410d9ffc964fb397_higher%20ed%20hero%20img.png"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <img
+        alt="icon"
+        src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a24d0e93ad13ce309c206d_transition%20lines.svg"
+      />
+      <section>
+        <div className="reimagine-cont">
+          <div className="re-header-box">
+            <div className="header">
+              Mental Health, Reimagined for Students.
+            </div>
+            <div className="re-header-about">
+              We understand that addressing mental health concerns can be
+              overwhelming. Connect your students with resources on campus that
+              meet their personal needs, fits their budget, and accommodates
+              their busy schedules.
+            </div>
+          </div>
+          <div className="re-about-box">
+            <div className="re-grid">
+              <div className="grid-item">
+                <div className="re-icon">
+                  <img
+                    alt="icon"
+                    className="grid-icon"
+                    src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a25c6e6eca503f93698031_icon%20_puzzle_.svg"
+                  />
+                </div>
+                <div className="re-about-body">
+                  <div className="re-title">
+                    Complements <br></br>Existing Resources
+                  </div>
+                  <div className="re-about">
+                    Enhance and augment the services already provided by your
+                    university. We work closely with university staff to ensure
+                    seamless integration of our software.
+                  </div>
+                </div>
+              </div>
+              <div className="grid-item">
+                <div className="re-icon">
+                  <img
+                    alt="icon"
+                    className="grid-icon"
+                    src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a25c6e0777a6887f8d6e05_icon%20_bar%20chart_.svg"
+                  />
+                </div>
+                <div className="re-about-body">
+                  <div className="re-title">
+                    Evidence Based & <br></br>Data Driven
+                  </div>
+                  <div className="re-about">
+                    We employ rigorous research methodologies to develop our
+                    platform. Our interventions are supported by scientific
+                    evidence and validated studies.
+                  </div>
+                </div>
+              </div>
+              <div className="grid-item">
+                <div className="re-icon">
+                  <img
+                    alt="icon"
+                    className="grid-icon"
+                    src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64c0b5aa7e8881a7475e0f79_icon%20_happy.png"
+                  />
+                </div>
+                <div className="re-about-body">
+                  <div className="re-title">
+                    Keep Students Engaged & <br></br>Improve Outcomes
+                  </div>
+                  <div className="re-about">
+                    Through gamification, progress tracking, and tailored
+                    content, enjoy a dynamic user experience that encourages
+                    sustained engagement.
+                  </div>
+                </div>
+              </div>
+              <div className="grid-item">
+                <div className="re-icon">
+                  <img
+                    alt="icon"
+                    className="grid-icon"
+                    src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64c0b5ac3cd3a8d0d1fb02e0_icon%20_ai.png"
+                  />
+                </div>
+                <div className="re-about-body">
+                  <div className="re-title">
+                    AI Algorithm for <br></br>Matched Results
+                  </div>
+                  <div className="re-about">
+                    By analyzing user data, our algorithms can dynamically
+                    generate personalized resources and interventions, providing
+                    students with support that is relevant to their specific
+                    mental health concerns and goals.
+                  </div>
+                </div>
+              </div>
+              <div className="grid-item">
+                <div className="re-icon">
+                  <img
+                    className="grid-icon"
+                    src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a25c6e6eca503f93698022_icon%20_secure.svg"
+                    alt="icon"
+                  />
+                </div>
+                <div className="re-about-body">
+                  <div className="re-title">
+                    Secure & <br></br>HIPAA Compliant
+                  </div>
+                  <div className="re-about">
+                    Maintain the trust and confidentiality necessary for
+                    students to seek support for their mental health needs with
+                    our HIPAA compliant platform.
+                  </div>
+                </div>
+              </div>
+              <div className="grid-item">
+                <div className="re-icon">
+                  <img
+                    alt="icon"
+                    className="grid-icon"
+                    src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64c0b5aa17a24e60d148511a_icon%20_budget.png"
+                  />
+                </div>
+                <div className="re-about-body">
+                  <div className="re-title">
+                    Custom Fit for<br></br>Budget and Needs
+                  </div>
+                  <div className="re-about">
+                    Mental health support should be accessible to all,
+                    regardless of budget constraints. We offer flexible pricing
+                    options and customizable plans, allowing universities to
+                    tailor our services to fit their budgetary needs
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="student">
+          <div className="student-box">
+            <div className="he-title2">The Student Journey Today</div>
+            <div className="he-title3">
+              In the current educational landscape there is a strong emphasis on
+              mental health, providing a wealth of resources. However, this
+              abundance of options and lack of guidance leave students grappling
+              with choice-paralysis. We provide a solution by simplifying access
+              to the help they truly need.
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="offer">
+          <div className="he-title2">What We Offer</div>
+          <div className="he-title3">
+            OOTify provides personalized recommendations to guide students
+            towards comprehensive mental health care. From HIPAA-compliant
+            tele-therapy to campus resources and our free mental health fluency
+            course, we empower students to thrive while optimizing institutional
+            support.
+          </div>
+          <div className="offer-box">
+            <div className="offer-item">
+              <img
+                className="offer-icon"
+                alt="icon"
+                src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a2650e9af1f6039ec1055f_%F0%9F%A6%86%20icon%20_hand%20strong_.svg"
+              />
+              <div className="offer-text">Resilience Module</div>
+            </div>
+            <div className="offer-item">
+              <img
+                className="offer-icon"
+                alt="icon"
+                src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a2650eb5f10321a6ec4ee9_%F0%9F%A6%86%20icon%20_forest_.svg"
+              />
+              <div className="offer-text">Campus Resources</div>
+            </div>
+            <div className="offer-item">
+              <img
+                className="offer-icon"
+                alt="icon"
+                src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a2650e3653fd9622524dcf_%F0%9F%A6%86%20icon%20_live_.svg"
+              />
+              <div className="offer-text">Live Events</div>
+            </div>
+            <div className="offer-item">
+              <img
+                className="offer-icon"
+                alt="icon"
+                src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a2650f63c3cc6de6656206_%F0%9F%A6%86%20icon%20_conversation_.svg"
+              />
+              <div className="offer-text">Peer-to-Peer Support</div>
+            </div>
+            <div className="offer-item">
+              <img
+                className="offer-icon"
+                alt="icon"
+                src="https://assets-global.website-files.com/64960fe9d01ccfa44ea57cae/64a2650e3a196175b6d50ef8_%F0%9F%A6%86%20icon%20_signature%20doc_.svg"
+              />
+              <div className="offer-text">Short Term Counseling</div>
+            </div>
           </div>
         </div>
       </section>
